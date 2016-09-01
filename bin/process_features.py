a8ed705db69a9f9842a4167b6ffec56d92b4d681
nastya <dyudina.anastasiya@yandex.ru>
2016-09-01 08:21:57 +0300

    feature agglomeration

--- bin/process_features.py
+++ bin/process_features.py
@@ -1,5 +1,5 @@
 import tarantool
-from sklearn import preprocessing, feature_extraction, cluster, metrics
+from sklearn import preprocessing, feature_extraction, cluster, metrics, random_projection, decomposition
 from sklearn.neighbors import NearestNeighbors
 from scipy import sparse
 import numpy
@@ -30,8 +30,8 @@ def _tfidf(features):
     tfidf = feature_extraction.text.TfidfTransformer()
     return tfidf.fit_transform(features)
     
-def _merge_features(count_features, word_features):
-    return sparse.hstack([count_features, word_features])
+def _merge_and_standardize_features(count_features, word_features):
+    return preprocessing.StandardScaler(with_mean=False).fit_transform(sparse.hstack([count_features, word_features]))
  
 def _find_neighbours(x, y):
     nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(x)
@@ -53,15 +53,24 @@ def _save_processed_features2tnt(y, x, tnt):
             print(e)
             pass #its ok to loose some wines
 
+def _convert2dense(x):
+    try:
+        return x.toarray()
+    except AttributeError:
+        return x
+        
 def _evaluate_clustering(x, labels):
-    return metrics.silhouette_score(x, labels, metric='euclidean')
+    try:
+        return metrics.silhouette_score(x, labels, metric='euclidean')
+    except ValueError: #too min labels
+        return 0
    
 def _test_cluster_algorithms(x, y):
     # KMeans/Agglomerative
     for n_clusters in range(2, 11):
         kmeans_model = cluster.KMeans(n_clusters=n_clusters).fit(x)
         print('kmeans accuracy for # clusters{}: {}'.format(n_clusters, _evaluate_clustering(x, kmeans_model.labels_)))
-        agglomerationg_model = cluster.AgglomerativeClustering(n_clusters=n_clusters).fit(x.toarray())
+        agglomerationg_model = cluster.AgglomerativeClustering(n_clusters=n_clusters).fit(_convert2dense(x))
         print('agglomerationg accuracy for # clusters{}: {}'.format(
             n_clusters, 
             _evaluate_clustering(x, agglomerationg_model.labels_)))
@@ -75,7 +84,7 @@ def _test_cluster_algorithms(x, y):
             _evaluate_clustering(x, model.labels_)))
             
     #MeanShift
-    model = cluster.MeanShift().fit(x.toarray())
+    model = cluster.MeanShift().fit(_convert2dense(x))
     print('meanshoft accuracy for dumping: {}'.format(
            _evaluate_clustering(x, model.labels_)))
      
@@ -85,11 +94,11 @@ def _test_cluster_algorithms(x, y):
         for min_samples in range(5, 20):
             model = cluster.DBSCAN(eps=eps).fit(x)
             n_clusters_ = len(set(model.labels_)) - (1 if -1 in model.labels_ else 0)
-            print('dbscan N clusters: {} 4 min_samples = {}'.format(n_clusters_, min_samples))
-            if n_clusters_ == 0: continue
+            print('dbscan N clusters: {} for min_samples = {}'.format(n_clusters_, min_samples))
             print('dbscan accuracy for eps: {} and min_samples == {}'.format(
                 eps,  
-                _evaluate_clustering(x, min_samples, model.labels_)))
+                min_samples,
+                _evaluate_clustering(x, model.labels_)))
             
     #Birch
     for threshold in range(1, 7):
@@ -99,7 +108,21 @@ def _test_cluster_algorithms(x, y):
         print('birch accuracy for threshold: {} == {}'.format(
             threshold,  
             _evaluate_clustering(x, model.labels_)))
-                            
+
+def _test_feature_extraction_algorithms(x, y):
+    feature_selectors = {
+        'PCA': decomposition.PCA(),
+        'SparseRandomProjection': random_projection.SparseRandomProjection(eps=0.35),
+        'GaussianRandomProjection': random_projection.GaussianRandomProjection(eps=0.35),
+    }  
+    for n_clusters in range(2, 10):
+        feature_selectors['FeatureAgglomeration: {}'.format(n_clusters)] = cluster.FeatureAgglomeration(n_clusters=n_clusters)  
+        
+    for name, selector in feature_selectors.items():
+        x_new = selector.fit_transform(x.toarray())
+        print('\n\n FEATURES SELECTED WITH: {}'.format(name))
+        _test_cluster_algorithms(x_new, y)
+                          
 def process_features():
     tnt = tarantool.connect(**TARANTOOL_CONNCTION)
     features = tnt.call('feature.get_feature_table', [[]]).data
@@ -109,12 +132,12 @@ def process_features():
 
     _scale_count_features(count_features)
     word_features = _tfidf(word_features)
-    x = _merge_features(count_features, word_features)
-    _save_processed_features2tnt(y, x, tnt)
+    x = _merge_and_standardize_features(count_features, word_features)
+    #_save_processed_features2tnt(y, x, tnt)
     
-    _test_cluster_algorithms(x, y)
+    #_test_cluster_algorithms(x, y)
     
-    #_test_feature_extraction_algorithms(x, y)
+    #_test_feature_extraction_algorithms(x, y) --> 4, 5, 6, 7 by Feauture agglomeration
     #_find_neighbours(x, y)    
 
 if __name__ == '__main__':
